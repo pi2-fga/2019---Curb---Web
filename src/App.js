71ad90d587f9820f8b91937c4e951455ac54b71a
IolaneAndrade <andrade.icaa@gmail.com>
2019-06-01 14:59:33 -0300

    resolved merge conflicts

--- src/App.js
+++ src/App.js
@@ -11,12 +11,9 @@ import "muicss/react";
 import 'antd/dist/antd.less';
 import MyHeader     from "./components/Header";
 import GeneralStatistics from "./pages/GeneralStatistics";
-<<<<<<< HEAD
 import UserRegister from "./pages/UserRegister";
 import Login from "./pages/Login";
-=======
 import Curb from "./pages/Curb";
->>>>>>> 258d029cafe73af57a497f5269f7fa5165e89539
 
 const { Content } = Layout;
 
